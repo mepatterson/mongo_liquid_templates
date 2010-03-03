be8bdaa3eceeede23c463f8827065ff1d8439526
Matt E. Patterson <mpatterson@Matt-E-Pattersons-Mac-Pro.local>
2010-03-03 11:55:35 -0600

    better error msg

--- lib/database_file_system.rb
+++ lib/database_file_system.rb
@@ -12,7 +12,7 @@ module MongoLiquidTemplates
       if mongotmpl = DynamicTemplate.find_by_path(template_path)
         mongotmpl.parsed ? Marshal.load(mongotmpl.parsed) : mongotmpl.body
       else
-        raise "Could not find template '#{template_path}'"
+        raise "Could not find template '#{template_path}'; maybe you need to add this template to mongo's dynamic_templates collection?"
       end
     end
     
